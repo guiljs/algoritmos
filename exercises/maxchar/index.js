e5020bcfb68a5dae636beedac19ef4bfa11c874d
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-11-26 20:31:51 -0200

    maxChar solucionado

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,36 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (let x of str) {
+    chars[x] = chars[x]+1 || 1;
+/*
+    if(chars[x])
+    {
+        chars[x]++;
+    }
+    else
+    {
+        chars[x]=1;
+    }
+    */
+    
+  }
+  console.log(chars);
+
+  let iBigger=0;
+  let cBigger='';
+
+  for(let char in chars){
+      if(iBigger < chars[char])
+      {
+        iBigger=chars[char];
+        cBigger=char;
+      }
+  }
+  return cBigger;
+
+}
 
 module.exports = maxChar;
