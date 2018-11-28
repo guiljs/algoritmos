8b00c700978ae318631879eb4b50fc9cde67b831
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-11-28 07:49:13 -0200

    Chucked solution professor

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,28 +8,43 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  let newArray = [];
-  let subArray = [size];
-  let index = 0;
-  let newArrayIndex = 0;
+// function chunk(array, size) {
+//   let newArray = [];
+//   let subArray = [size];
+//   let index = 0;
+//   let newArrayIndex = 0;
 
-  for (const x of array) {
-    if (index < size) {
-      subArray[index] = x;
-      index++;
-    } else {
-      index = 0;
-      newArray[newArrayIndex] = subArray;
-      newArrayIndex++;
-      subArray = [size];
-      subArray[index] = x;
-      index++;
+//   for (const x of array) {
+//     if (index < size) {
+//       subArray[index] = x;
+//       index++;
+//     } else {
+//       index = 0;
+//       newArray[newArrayIndex] = subArray;
+//       newArrayIndex++;
+//       subArray = [size];
+//       subArray[index] = x;
+//       index++;
+//     }
+//   }
+//   newArray[newArrayIndex] = subArray;
+
+//   return newArray;
+// }
+
+function chunk(array, size) {
+  let chunked = [];
+
+  for (let element of array) {
+    let last = chunked[chunked.length - 1]; //Pega o último array dentro do array chunked
+
+    if (!last || last.length === size) {
+      chunked.push([element]); //Se não existir ou já ter o tamanho do parametro size, cria um novo array (subarray) que já contém o elemento atual da iteração.
+    } else{
+        last.push(element); //Se existir e ainda não estiver no tamanho maximo permitido, adiciono no subarray o elemento. 
     }
   }
-  newArray[newArrayIndex] = subArray;
 
-  return newArray;
+  return chunked;
 }
-
 module.exports = chunk;
