844dbc81d62169fdad8d1cffcfe37d0dc9d8bcdd
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-12-04 08:14:37 -0200

    Capitalize #2 solution - not so legible

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,30 +8,41 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  const words = [];
+  let result = str[0].toUpperCase();
 
-  for (let word of str.split(" ")) {
-    words.push(word[0].toUpperCase() + word.slice(1));
+  for (let index = 1; index < str.length; index++) {
+    if (str[index - 1] === " ") {
+      result += str[index].toUpperCase();
+    } else {
+      result += str[index];
+    }
   }
-  return words.join(" ");
-  //   let newStr = "";
-  //   for (let x of str.split(" ")) {
-  //     newStr += x[0].toUpperCase() + x.slice(1);
+  return result;
 
-  //     if (newStr.length !== str.length) {
-  //       newStr += " ";
-  //     }
+  //   const words = [];
+
+  //   for (let word of str.split(" ")) {
+  //     words.push(word[0].toUpperCase() + word.slice(1));
   //   }
-  //   return newStr;
-  //   // let newStr="";
-  //   // for(let x of str.slice(0))
-  //   // {
-  //   //     newStr+=x.split(0)[0].toUpperCase()
+  //   return words.join(" ");
+  //   //   let newStr = "";
+  //   //   for (let x of str.split(" ")) {
+  //   //     newStr += x[0].toUpperCase() + x.slice(1);
 
-  //   //     for(let y of x.split(' '){
-  //   //         if(x.split())
+  //   //     if (newStr.length !== str.length) {
+  //   //       newStr += " ";
   //   //     }
-  //   // }
+  //   //   }
+  //   //   return newStr;
+  //   //   // let newStr="";
+  //   //   // for(let x of str.slice(0))
+  //   //   // {
+  //   //   //     newStr+=x.split(0)[0].toUpperCase()
+
+  //   //   //     for(let y of x.split(' '){
+  //   //   //         if(x.split())
+  //   //   //     }
+  //   //   // }
 }
 
 module.exports = capitalize;
